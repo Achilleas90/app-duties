7d860968f6f3025e82551e8ec9a1735c6e8e7890
Achilleas90 <achilleasnake@gmail.com>
2025-07-23 14:28:54 +0300

    Fixed the duties staff selector

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def staff_duties(staff_id):
 @app.route('/duties')
 def duties():
     month = request.args.get('month')
-    staff = {s.id: s for s in Staff.query.all()}
+    staff = Staff.query.order_by(Staff.rank, Staff.name).all()
 
     # Get all duties, or filter by selected month
     duties_query = Duty.query
